d933bd206ce637380d8a5e72b1551beb3acca610
21世纪末白银闪\y!n\Desktop <e1155389@u.nus.edu>
2025-03-15 14:12:59 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Message from "./Message";
+
+
+function App() {
+
+  
+  return <div><Message></Message></div>;
+
+  
+
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,14 @@
+
+
+function Message() {
+    const name = '';
+
+    if (name)
+    return <h1>Hello {name} </h1>;
+    return <h1>Hello World</h1>;
+
+    //javascript xml
+
+}
+
+export default Message;
